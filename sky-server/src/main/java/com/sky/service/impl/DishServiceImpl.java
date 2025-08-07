ec15ce1a19262f8c59d1c96a99ee2709745c814c
Qzat <lansedexuan@gmail.com>
2025-08-08 00:29:20 +0800

    菜品查询

---
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -0,0 +1,72 @@
+package com.sky.service.impl;
+
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.entity.Dish;
+import com.sky.entity.DishFlavor;
+import com.sky.mapper.DishFlavorMapper;
+import com.sky.mapper.DishMapper;
+import com.sky.result.PageResult;
+import com.sky.service.DishService;
+import com.sky.vo.DishVO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Slf4j//日志
+public class DishServiceImpl implements DishService {
+    @Autowired//自动注入
+    private DishMapper dishMapper;
+
+    @Autowired
+    private DishFlavorMapper dishFlavorMapper;
+    /**
+     * 新增菜品，同时保存对应的口味数据
+     * @param dishDTO
+     */
+    @Transactional//事务
+    public void saveWithFlavor(DishDTO dishDTO) {
+
+        Dish dish = new Dish();
+
+        BeanUtils.copyProperties(dishDTO, dish);//属性拷贝
+
+        //向菜品表插入一条数据
+        dishMapper.insert(dish);
+
+        //获取insert语句生成的主键值
+        Long id = dish.getId();
+
+        //向口味表插入多条数据
+        List<DishFlavor> flavors = dishDTO.getFlavors();
+        if(flavors != null && flavors.size() > 0){
+            flavors.forEach(dishFlavor -> {
+                dishFlavor.setDishId(id);
+            });
+            dishFlavorMapper.insertBatch(flavors);
+        }
+    }
+
+    /**
+     * 菜品分页查询
+     * @param dishPageQueryDTO
+     * @return
+     */
+    @Override
+    public PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO) {
+        PageHelper.startPage(dishPageQueryDTO.getPage(), dishPageQueryDTO.getPageSize());//页码 每页记录数
+        Page<DishVO> page = dishMapper.pageQuery(dishPageQueryDTO);
+
+        long total = page.getTotal();
+        List<DishVO> records = page.getResult();
+
+        return new PageResult(total, records);
+    }
+}
